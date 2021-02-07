a32c27c80df900ba22439520829f9e29e4117027
Konstantinos <kongallis@gmail.com>
2021-02-07 18:46:53 +0200

    Add Sign in, Sign out and Login buttons to the navigation bar dynamically.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
-  
+
   root to: "main#index"
 
   get "about-us", to: "about#index", as: :about
 
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
+
+  # SESSION
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   get "first", to: "first#index"
 end
