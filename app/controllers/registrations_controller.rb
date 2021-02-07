9c2d69d5b9c1780270101ec30edd2931a251d99c
Konstantinos <kongallis@gmail.com>
2021-02-07 17:44:13 +0200

    Add session cookies when a user registers

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -8,6 +8,7 @@ class RegistrationsController < ApplicationController
 
         @user = User.new(user_params)
         if @user.save
+            session[:user_id] = @user.id
             redirect_to root_path, notice: "Successfully created account"
         else
             render :new
