7137cce4ab907ec95af9d4bce1dd49023d6393ea
Konstantinos <kongallis@gmail.com>
2021-02-14 14:04:04 +0200

    Add and set up OmniAuth

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,4 +10,21 @@ class PasswordResetsController < ApplicationController
         end
         redirect_to root_path, notice: "If an account with that email was found, we have sent you a link to reset your password"
     end
+
+    def edit
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redict_to sign_in_path, alert: "Your token has expired. Please try again."
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Your password was reset successfully. Please sign in."
+        end
+    end
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
+    end
 end
